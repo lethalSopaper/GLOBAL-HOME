eda19d288a0cad2fe1b078e7bba08abc177ca7e2
luis <2bluisugartechea@gmail.com>
2024-12-04 22:43:36 -0600

    carga inicial terminada

---
+++ carga_inicial/COMPRA.sql
@@ -0,0 +1,11 @@
+-- comision del 10%
+-- Pagos incompletos
+insert into COMPRA (vivienda_venta_id, clabe_interbancaria, precio_final, comision, usuario_id) values (37, 776438989468521984, 4623567541, 462356754.1, 81);
+insert into COMPRA (vivienda_venta_id, clabe_interbancaria, precio_final, comision, usuario_id) values (38, 139846684865163894, 5961773702, 596177370.2, 82);
+insert into COMPRA (vivienda_venta_id, clabe_interbancaria, precio_final, comision, usuario_id) values (39, 091823082013809801, 7461112592, 746111259.2, 83);
+insert into COMPRA (vivienda_venta_id, clabe_interbancaria, precio_final, comision, usuario_id) values (40, 987394523059610134, 6797568160, 679756816, 84);
+insert into COMPRA (vivienda_venta_id, clabe_interbancaria, precio_final, comision, usuario_id) values (41, 077058610295758142, 4289979960, 428997996, 85);
+-- Pagos completos
+insert into COMPRA (vivienda_venta_id, clabe_interbancaria, precio_final, comision, usuario_id) values (42, 123456789012345678, 1026822992, 102682299.2, 86);
+insert into COMPRA (vivienda_venta_id, clabe_interbancaria, precio_final, comision, usuario_id) values (43, 123456789012345678, 4016324052, 401632405.2, 87);
+insert into COMPRA (vivienda_venta_id, clabe_interbancaria, precio_final, comision, usuario_id) values (44, 123456789012345678, 5638030330, 563803033, 88);
---
+++ carga_inicial/PAGO.sql
@@ -0,0 +1,26 @@
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (1, 38, to_date('15-06-2024', 'dd-mm-yyyy'), 596177370.2, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (1, 39, to_date('18-09-2024', 'dd-mm-yyyy'), 746111259.2, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (2, 39, to_date('24-10-2024', 'dd-mm-yyyy'), 746111259.2, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (1, 40, to_date('01-07-2024', 'dd-mm-yyyy'), 679756816, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (2, 40, to_date('15-07-2024', 'dd-mm-yyyy'), 679756816, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (1, 41, to_date('15-08-2024', 'dd-mm-yyyy'), 428997996, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (2, 41, to_date('15-09-2024', 'dd-mm-yyyy'), 428997996, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (3, 41, to_date('15-10-2024', 'dd-mm-yyyy'), 428997996, EMPTY_BLOB());
+-- Completado en 2 pagos
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (1, 42, to_date('01-01-2024', 'dd-mm-yyyy'), 513411496, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (2, 42, to_date('18-01-2024', 'dd-mm-yyyy'), 513411496, EMPTY_BLOB());
+-- Completado en 3 pagos
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (1, 43, to_date('01-02-2024', 'dd-mm-yyyy'), 1338774684, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (2, 43, to_date('15-02-2024', 'dd-mm-yyyy'), 1338774684, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (3, 43, to_date('29-02-2024', 'dd-mm-yyyy'), 1338774684, EMPTY_BLOB());
+-- Completado en 10 pagos
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (1, 44, to_date('01-03-2024', 'dd-mm-yyyy'), 563803033, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (2, 44, to_date('15-03-2024', 'dd-mm-yyyy'), 563803033, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (3, 44, to_date('29-03-2024', 'dd-mm-yyyy'), 563803033, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (4, 44, to_date('12-04-2024', 'dd-mm-yyyy'), 563803033, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (5, 44, to_date('26-04-2024', 'dd-mm-yyyy'), 563803033, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (6, 44, to_date('10-05-2024', 'dd-mm-yyyy'), 563803033, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (7, 44, to_date('24-05-2024', 'dd-mm-yyyy'), 563803033, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (8, 44, to_date('07-06-2024', 'dd-mm-yyyy'), 563803033, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (9, 44, to_date('21-06-2024', 'dd-mm-yyyy'), 563803033, EMPTY_BLOB());
+insert into PAGO (num_pago, vivienda_venta_id, fecha_pago, importe, recibo) values (10, 44, to_date('05-07-2024', 'dd-mm-yyyy'), 563803033, EMPTY_BLOB());
