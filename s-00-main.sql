e0d89b6a167d70e18085028639ead2f3cb1f00f1
hansmarcus <hansmarcus14@gmail.com>
2024-12-05 18:38:55 -0600

    avance en el s-00-main.sql

---
+++ s-00-main.sql
@@ -0,0 +1,32 @@
+-- Autor: Tepal Briseño Hansel Yael y Ugartechea González Luis Antonio
+-- Fecha: 05/12/2024
+-- Descripción: Archivo main para crear objetos
+
+prompt llamando al script s-01-usuarios.sql
+
+start s-01-usuarios.sql
+
+prompt llamando al script s-02-entidades.sql
+
+start s-02-entidades.sql
+
+prompt llamando al script s-05-secuencias.sql
+
+start s-05-secuencias.sql
+
+prompt llamando al script s-06-indices.sql
+
+start s-06-indices.sql
+
+prompt llamando al script s-07-sinonimos.sql
+
+start s-07-sinonimos.sql
+
+prompt llamando al script s-08-vistas.sql
+
+start s-08-vistas.sql
+
+--Carga inicial
+prompt llamando al script s-09-carga-inicial.sql
+
+start s-09-carga-inicial.sql
