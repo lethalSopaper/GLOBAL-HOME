e0bc2a22fd1b27765a80d8a206ece5f6389cf411
hansmarcus <hansmarcus14@gmail.com>
2024-12-05 23:16:56 -0600

    tabla externa funcional. falta llenar bien el csv para datos de prueba

--- s-04-tablas-externas.sql
+++ s-04-tablas-externas.sql
@@ -5,19 +5,28 @@
 -- Conectando con el usuario sys
 connect sys/system1@htblugbd_s2 as sysdba
 
-create directory logs_dir as '/unam/bd/proyecto/GLOBAL-HOME/logs';
+-- Crear el directorio si no existe
+begin
+    execute immediate 'create directory logs_dir as ''/unam/bd/proyecto/GLOBAL-HOME/logs''';
+exception
+    when others then
+        if sqlcode != -955 then
+            raise;
+        end if;
+end;
+/
 
 -- Conceder permisos al usuario
 grant read, write on directory logs_dir to tu_proy_admin;
 
 -- Conectando con el usuario tu_proy_admin
-connect tu_proy_admin/hansel@htblugbd_s2
+connect tu_proy_admin/1234@htblugbd_s2
 
 -- Eliminar la tabla logs_ext si ya existe
 begin
     execute immediate 'drop table logs_ext';
-    exception 
-        when others then null;
+exception 
+    when others then
         if sqlcode != -942 then
             raise;
         end if;
@@ -26,12 +35,12 @@ end;
 
 -- Crear la tabla externa para los logs de actividades
 create table logs_ext (
-    evento_id number(10,0) primary key,
+    evento_id number(10,0),
     usuario_id number(10,0),
     accion varchar2(20),
     tabla_afectada varchar2(30),
     detalle_accion varchar2(1000),
-    fecha_evento timestamp default systimestamp,
+    fecha_evento timestamp,
     valor_anterior varchar2(1000), -- solo actualizaciones o eliminaciones
     valor_nuevo varchar2(1000) -- solo actualizaciones o inserciones
 )
@@ -42,7 +51,7 @@ organization external (
         records delimited by newline
         badfile 'logs_ext_bad.log' 
         logfile 'logs_ext.log' 
-        field terminated by ',' 
+        fields terminated by ',' 
         lrtrim 
         missing field values are null 
         (
@@ -50,12 +59,12 @@ organization external (
             usuario_id,
             accion,
             tabla_afectada,
-            mensaje,
-            fecha_evento timestamp "dd/mm/yyyy hh24:mi:ss.ff",
+            detalle_accion,
+            fecha_evento char date_format timestamp mask "dd/mm/yyyy hh24:mi:ss.ff3",
             valor_anterior,
-            valor_nuevo, 
+            valor_nuevo
         )
     )
     location ('logs_ext.csv')
 )
-reject limit unlimited; 1  
+reject limit unlimited;
