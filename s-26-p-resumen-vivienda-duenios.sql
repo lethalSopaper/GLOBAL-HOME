d68b7b1c1c5f04100d2ef00040995d510afd5068
luis <2bluisugartechea@gmail.com>
2024-12-08 12:40:29 -0600

    cambio de nombres

--- s-26-p-resumen-vivienda-duenios.sql
+++ s-26-p-resumen-vivienda-duenios.sql
@@ -16,6 +16,8 @@ as
     --     join estatus_vivienda ev on v.estatus_vivienda_id = ev.estatus_vivienda_id;
     -- -- variable para almacenar un registro de vivienda
     -- v_vivienda cur_viviendas%rowtype;
+    -- variable para saber la cantidad de viviendas de vacaciones-renta
+    v_cantidad_viviendas_vacaciones_renta number := 0;
     -- variable para saber la cantidad de viviendas de vacaciones
     v_cantidad_viviendas_vacaciones number := 0;
     -- variable para saber la calificación promedio de las viviendas vacaciones
@@ -25,7 +27,37 @@ as
     -- variable para saber la cantidad de viviendas de venta
     v_cantidad_viviendas_venta number := 0;
 begin
-    -- Para cada dueño de vivienda
-    for 
+    -- loop para recorrer cada dueño de vivienda
+    for duenio in cur_duenios loop
+        -- Cantidad de viviendas de vacaciones y renta
+        select count(*) into v_cantidad_viviendas_vacaciones_renta
+        from vivienda
+        where v.usuario_duenio_id = duenio.usuario_duenio_id
+        and es_vacaciones = true
+        and es_renta = true
+        and es_venta = false;
+        -- Cantidad de viviendas de vacaciones
+        select count(*) into v_cantidad_viviendas_vacaciones
+        from vivienda
+        where v.usuario_duenio_id = duenio.usuario_duenio_id
+        and es_vacaciones = true
+        and es_renta = false
+        and es_venta = false;
+        -- Cantidad de viviendas de renta
+        select count(*) into v_cantidad_viviendas_renta
+        from vivienda
+        where v.usuario_duenio_id = duenio.usuario_duenio_id    
+        and es_vacaciones = false
+        and es_renta = true
+        and es_venta = false;
+        -- Cantidad de viviendas de venta
+        select count(*) into v_cantidad_viviendas_venta
+        from vivienda
+        where v.usuario_duenio_id = duenio.usuario_duenio_id
+        and es_vacaciones = false
+        and es_renta = false
+        and es_venta = true;
+        
+    end loop;
 end;
 /
