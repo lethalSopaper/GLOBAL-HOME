38eb5e91a492a7ac532f943b030c1d4a57fb875f
luis <2bluisugartechea@gmail.com>
2024-12-08 19:18:32 -0600

    procedimientos sin errores, no sé escribir

--- s-22-p-resumen-vivienda-duenios.sql
+++ s-22-p-resumen-vivienda-duenios.sql
@@ -1,7 +1,3 @@
--- Autor: Tepal Briseño Hansel Yael y Ugartechea González Luis Antonio
---Fecha: 07/12/2024
---Descripción: Este procedimiento busca imprimir un resumen de calificaciones y ganancias para los dueños de viviendas teniendo en cuenta todos lo tipos de viviendas que poseen.
-
 create or replace procedure p_resumen_viviendas
 as
     -- cursor para seleccionar todas las viviendas y dueños
@@ -18,33 +14,31 @@ as
     v_cantidad_viviendas_renta number := 0;
     -- variable para saber la cantidad de viviendas de venta
     v_cantidad_viviendas_venta number := 0;
-    -- variabele para almacenar la calificacón promedio de las viviendas de vacaciones
-    v_calificacion_promedio number := 0;
     -- variable para almacenar las ganancias de alquileres
-    v_gananacias_alquileres number := 0;
+    v_ganancias_alquileres number := 0;
     -- Variables para almacenar las ganancias de rentas
-    v_gananacias_rentas number := 0;
+    v_ganancias_rentas number := 0;
     -- variable para almacenar las ganancias de compras
-    v_gananacias_compras number := 0;
+    v_ganancias_compras number := 0;
     -- variable para almacenar las ganancias totales
-    v_gananacias_totales number := 0;
+    v_ganancias_totales number := 0;
     -- cursor para seleccionar todas las viviendas renta de un dueño
 begin
     -- Para cada dueño de vivienda
     for duenio in cur_duenios loop
-        v_gananancias_alquileres := 0;
-        v_gananancias_rentas := 0;
-        v_gananancias_compras := 0;
+        v_ganancias_alquileres := 0;
+        v_ganancias_rentas := 0;
+        v_ganancias_compras := 0;
         -- Cantidad de viviendas de vacaciones y renta
         select count(*) into v_cantidad_viviendas_vacaciones_renta
-        from vivienda
+        from vivienda v
         where v.usuario_duenio_id = duenio.usuario_duenio_id
         and es_vacaciones = true
         and es_renta = true
         and es_venta = false;
         -- Cantidad de viviendas de vacaciones
         select count(*) into v_cantidad_viviendas_vacaciones
-        from vivienda
+        from vivienda v
         where v.usuario_duenio_id = duenio.usuario_duenio_id
         and es_vacaciones = true
         and es_renta = false
@@ -58,35 +52,34 @@ begin
         where v.usuario_duenio_id = duenio.usuario_duenio_id;
         -- Cantidad de viviendas de renta
         select count(*) into v_cantidad_viviendas_renta
-        from vivienda
+        from vivienda v
         where v.usuario_duenio_id = duenio.usuario_duenio_id    
         and es_vacaciones = false
         and es_renta = true
         and es_venta = false;
         -- Cantidad de viviendas de venta
         select count(*) into v_cantidad_viviendas_venta
-        from vivienda
+        from vivienda v
         where v.usuario_duenio_id = duenio.usuario_duenio_id
         and es_vacaciones = false
         and es_renta = false
-        and es_venta = true
-        where v.usuario_duenio_id = duenio.usuario_duenio_id;
+        and es_venta = true;
         -- Ganancias de alquiler
-        select sum(a.costo_total) into v_gananacias_alquileres
+        select sum(a.costo_total) into v_ganancias_alquileres
         from vivienda v
         join vivienda_vacaciones vv on v.vivienda_id = vv.vivienda_vacaciones_id
         join alquiler a on vv.vivienda_vacaciones_id = a.vivienda_vacaciones_id
         where v.usuario_duenio_id = duenio.usuario_duenio_id;
         -- Ganancias de renta
-        p_calcular_ganancia_total_rentas(v_gananacias_rentas, duenio.usuario_duenio_id);
+        p_calcular_ganancia_total_rentas(v_ganancias_rentas, duenio.usuario_duenio_id);
         -- Ganancias de compra
-        select sum(p.importe) into v_gananacias_compras
+        select sum(p.importe) into v_ganancias_compras
         from vivienda v
         join vivienda_venta vv on v.vivienda_id = vv.vivienda_venta_id
         join pago p on vv.vivienda_venta_id = p.vivienda_venta_id
         where v.usuario_duenio_id = duenio.usuario_duenio_id;
         -- Ganancias totales
-        v_gananacias_totales := v_gananacias_alquileres + v_gananacias_rentas + v_gananacias_compras;
+        v_ganancias_totales := v_ganancias_alquileres + v_ganancias_rentas + v_ganancias_compras;
         -- Se imprime el resumen
         dbms_output.put_line('Dueño: ' || duenio.usuario_duenio_id);
         dbms_output.put_line('Cantidad de viviendas de vacaciones y renta: ' || v_cantidad_viviendas_vacaciones_renta);
@@ -95,7 +88,7 @@ begin
         dbms_output.put_line('Cantidad de viviendas de renta: ' || v_cantidad_viviendas_renta);
         dbms_output.put_line('Cantidad de viviendas de venta: ' || v_cantidad_viviendas_venta);
         -- Imprimir ganancias totales
-        dbms_output.put_line('Ganancias totales: ' || v_gananacias_totales);
+        dbms_output.put_line('Ganancias totales: ' || v_ganancias_totales);
         dbms_output.put_line('---------------------------------------------------------------------');
     end loop;
 exception
