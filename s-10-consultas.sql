8d523ffb5c2f966e8a4e5de8798a5a0c20ab0ed3
luis <2bluisugartechea@gmail.com>
2024-12-09 12:16:59 -0600

    Commit final

--- s-10-consultas.sql
+++ s-10-consultas.sql
@@ -98,8 +98,3 @@ select fecha_evento, usuario, accion, valor_anterior, valor_nuevo
 from logs_ext
 where tabla_afectada = 'ALQUILER'
 and accion = 'UPDATE';
-
--- CONSULTA 6:
-/* Se desea realizar una auditoria a los usuarios que han modimifado la tabla alquiler, 
-se necesita saber el nombre, apellido paterno, apellido materno (de existir) y el email de los usuarios.
-*
