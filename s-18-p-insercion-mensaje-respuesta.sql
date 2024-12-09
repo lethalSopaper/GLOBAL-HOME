b8560c9e20408325ec373c0b43017ab4e04c0485
luis <2bluisugartechea@gmail.com>
2024-12-08 21:06:26 -0600

    termino pruebas de insercion de respuesta

--- s-18-p-insercion-mensaje-respuesta.sql
+++ s-18-p-insercion-mensaje-respuesta.sql
@@ -12,7 +12,26 @@ create or replace procedure p_insercion_mensaje_respuesta(
     p_usuario_id in mensaje.usuario_id%type,    
     p_vivienda_id in mensaje.vivienda_id%type
 )as
+    -- Variables
+    -- variable para verificar si el mensaje al que se está respondiendo existe
+    v_mensaje_respondido_id mensaje.mensaje_id%type;
+    -- variable para verificar si el mensaje al que se está respondiendo tiene respuesta
+    v_verificacion_respuesta mensaje.mensaje_id%type;
 begin
+    -- Primero se verifica si el mensaje al que se está respondiendo existe
+    select mensaje_id into v_mensaje_respondido_id
+    from mensaje
+    where mensaje_id = p_mensaje_respondido_id;
+
+    select mensaje_respuesta_id into v_verificacion_respuesta
+    from mensaje
+    where mensaje_id = v_mensaje_respondido_id;
+
+    -- verificar si ya se respondio al mensaje
+    if v_verificacion_respuesta is not null then
+        raise_application_error(-20100, 'El mensaje al que se está respondiendo ya tiene respuesta');
+    end if;
+
     -- Se inserta el mensaje de respuesta
     insert into mensaje(mensaje_id, titulo, cuerpo, leido, fecha_envio, usuario_id, vivienda_id)
     values(p_mensaje_respuesta_id, p_titulo, p_cuerpo, false, sysdate, p_usuario_id, p_vivienda_id);
@@ -23,8 +42,13 @@ begin
     where mensaje_id = p_mensaje_respondido_id;
 
 exception
+    when NO_DATA_FOUND then
+        raise_application_error(-20200, 'El mensaje al que se está respondiendo no existe');
     when others then
-        raise_application_error(-30001, 'Error al insertar el mensaje de respuesta');
+        if sqlcode = -20100 then
+            raise_application_error(-20100, 'El mensaje al que se está respondiendo ya tiene respuesta');
+        end if;
+        raise_application_error(-20300, 'Error al insertar el mensaje de respuesta');
 end;
 /
 show errors;
--- s-19-p-insercion-mensaje-respuesta-prueba.sql
+++ s-19-p-insercion-mensaje-respuesta-prueba.sql
@@ -4,69 +4,116 @@
 
 set serveroutput on
 declare
-    v_mensaje_50 mensaje%rowtype;
-    v_mensaje_51 mensaje%rowtype;
-    v_mensaje_60 mensaje%rowtype;
-    v_mensaje_61 mensaje%rowtype;
-    v_mensaje_62 mensaje%rowtype;
+    v_mensaje_70 mensaje%rowtype;
+    v_mensaje_71 mensaje%rowtype;
+    v_mensaje_72 mensaje%rowtype;
 begin
     -- Se inserta un mensaje de prueba al que vamos a contestar
     insert into mensaje(mensaje_id, titulo, cuerpo, usuario_id, vivienda_id)
     values(50, 'titutlo_de_prueba_id_50', 'cuerpo_de_prueba_id_50',150, 1);
+    -- Caso 1: Se responde dos veces al mensaje con id 50
     begin
-        dbms_output.put_line('Prueba 1: Mensaje de respuesta para el mensaje con id 50');
+        dbms_output.put_line('Caso 1: Se responde dos veces al mensaje con id 50');
         p_insercion_mensaje_respuesta(50,
             51,
             'titulo_de_prueba_respuesta_id_51',
             'cuerpo_de_prueba_respuesta_id_51',
             149,
             1);
-        -- Mostrar el mensjae con id 50
-        select * into v_mensaje_50 from mensaje where mensaje_id = 50;
-        -- Mostrar el mensaje con id 51
-        select * into v_mensaje_51 from mensaje where mensaje_id = 51;
-        -- Para mostrar los mensajes
-        dbms_output.put_line('Mensaje respondido: ' || v_mensaje_50.mensaje_id || ' ' || v_mensaje_50.titulo || ' ' || v_mensaje_50.cuerpo || ' ' || v_mensaje_50.usuario_id || ' ' || v_mensaje_50.vivienda_id || ' ' || v_mensaje_50.mensaje_respuesta_id);
-        dbms_output.put_line('Mensaje respuesta: ' || v_mensaje_51.mensaje_id || ' ' || v_mensaje_51.titulo || ' ' || v_mensaje_51.cuerpo || ' ' || v_mensaje_51.usuario_id || ' ' || v_mensaje_51.vivienda_id);
-        dbms_output.put_line('Mensaje de respuesta insertado correctamente!');
+        p_insercion_mensaje_respuesta(50,
+            52,
+            'titulo_de_prueba_respuesta_id_52',
+            'cuerpo_de_prueba_respuesta_id_52',
+            149,
+            1);
+        dbms_output.put_line('Mensajes de respuesta insertado correctamente (ERROR)');
+        rollback;
     exception
         when others then
-            rollback;
-            dbms_output.put_line('Error al insertar el mensaje de respuesta');
+            if sqlcode = -20100 then
+                rollback;
+                dbms_output.put_line('Error esperado al insertar mensaje!' || chr(10) || sqlerrm);
+            elsif sqlcode = -20200 then
+                rollback;
+                dbms_output.put_line('No existe el mensaje al que se está respondiendo (ERROR)' || chr(10) || sqlerrm);
+            elsif sqlcode = -20300 then
+                rollback;
+                dbms_output.put_line('Error al insertar el mensaje de respuesta (ERROR)' || chr(10) || sqlerrm);
+            else
+                rollback;
+                dbms_output.put_line('Error al insertar el mensaje de respuesta_1 (ERROR)' || chr(10) || sqlerrm);
+            end if;
+        rollback;
     end;
-
-    -- Se inserta un mensaje de prueba al que vamos a contestar
-    insert into mensaje(mensaje_id, titulo, cuerpo, usuario_id, vivienda_id)
-    values(60, 'titutlo_de_prueba_id_51', 'cuerpo_de_prueba_id_51',150, 1);
+    -- Caso 2: Se responde a un mensaje que no existe
     begin
-        dbms_output.put_line('Prueba 2: Mensaje de respuesta para el mensaje con id 60');
+        dbms_output.put_line('Caso 2: Se responde a un mensaje que no existe');
         p_insercion_mensaje_respuesta(60,
             61,
             'titulo_de_prueba_respuesta_id_61',
             'cuerpo_de_prueba_respuesta_id_61',
             149,
             1);
-        p_insercion_mensaje_respuesta(61,
-            62,
-            'titulo_de_prueba_respuesta_id_62',
-            'cuerpo_de_prueba_respuesta_id_62',
-            149,
-            1);
-        -- Mostrar el mensjae con id 60
-        select * into v_mensaje_60 from mensaje where mensaje_id = 60;
-        -- Mostrar el mensaje con id 61
-        select * into v_mensaje_61 from mensaje where mensaje_id = 61;
-        -- Mostrar el mensaje con id 62
-        select * into v_mensaje_62 from mensaje where mensaje_id = 62;
-        -- Para mostrar los mensajes
-        dbms_output.put_line('Mensaje respondido: ' || v_mensaje_60.mensaje_id || ' ' || v_mensaje_60.titulo || ' ' || v_mensaje_60.cuerpo || ' ' || v_mensaje_60.usuario_id || ' ' || v_mensaje_60.vivienda_id || ' ' || v_mensaje_60.mensaje_respuesta_id);
-        dbms_output.put_line('Mensaje respuesta 1: ' || v_mensaje_61.mensaje_id || ' ' || v_mensaje_61.titulo || ' ' || v_mensaje_61.cuerpo || ' ' || v_mensaje_61.usuario_id || ' ' || v_mensaje_61.vivienda_id || ' ' || v_mensaje_61.mensaje_respuesta_id);
-        dbms_output.put_line('Mensaje respuesta 2: ' || v_mensaje_62.mensaje_id || ' ' || v_mensaje_62.titulo || ' ' || v_mensaje_62.cuerpo || ' ' || v_mensaje_62.usuario_id || ' ' || v_mensaje_62.vivienda_id);
-        dbms_output.put_line('Mensajes de respuesta insertado correctamente!');
+        dbms_output.put_line('Mensajes de respuesta insertado correctamente (ERROR)');
     exception
         when others then
-            rollback;
-            dbms_output.put_line('Error al insertar el mensaje de respuesta');
+            if sqlcode = -20100 then
+                rollback;
+                dbms_output.put_line('Se está respondiendo a un mensaje que ya existe (ERROR)' || chr(10) || sqlerrm);
+            elsif sqlcode = -20200 then
+                rollback;
+                dbms_output.put_line('Error esperado al insertar mensaje!' || chr(10) || sqlerrm);
+            elsif sqlcode = -20300 then
+                rollback;
+                dbms_output.put_line('Error al insertar el mensaje de respuesta (ERROR)' || chr(10) || sqlerrm);
+            else
+                rollback;
+                dbms_output.put_line('Error al insertar el mensaje de respuesta_1 (ERROR)' || chr(10) || sqlerrm);
+            end if;
+        rollback;
+    end;
+    -- Se inserta un mensaje de prueba al que vamos a contestar
+    insert into mensaje(mensaje_id, titulo, cuerpo, usuario_id, vivienda_id)
+    values(70, 'titutlo_de_prueba_id_61', 'cuerpo_de_prueba_id_61',150, 1);
+    -- Caso 3; Se reponde a un mensjae de manera adecuada
+    begin
+        dbms_output.put_line('Caso 3; Se reponde a un mensjae de manera adecuada');
+        p_insercion_mensaje_respuesta(70,
+            71,
+            'titulo_de_prueba_respuesta_id_71',
+            'cuerpo_de_prueba_respuesta_id_71',
+            149,
+            1);
+        p_insercion_mensaje_respuesta(71,
+            72,
+            'titulo_de_prueba_respuesta_id_72',
+            'cuerpo_de_prueba_respuesta_id_72',
+            149,
+            1);
+        dbms_output.put_line('Mensajes de respuesta insertado correctamente !');
+        -- Se imprimer el contenido de cada mensaje
+        select * into v_mensaje_70 from mensaje where mensaje_id = 70;
+        select * into v_mensaje_71 from mensaje where mensaje_id = 71;
+        select * into v_mensaje_72 from mensaje where mensaje_id = 72;
+        dbms_output.put_line('Mensaje 70: ' || v_mensaje_70.mensaje_id || ' ' || v_mensaje_70.titulo || ' ' || v_mensaje_70.cuerpo || ' ' || v_mensaje_70.usuario_id || ' ' || v_mensaje_70.vivienda_id || ' ' || v_mensaje_70.mensaje_respuesta_id);
+        dbms_output.put_line('Mensaje 71: ' || v_mensaje_71.mensaje_id || ' ' || v_mensaje_71.titulo || ' ' || v_mensaje_71.cuerpo || ' ' || v_mensaje_71.usuario_id || ' ' || v_mensaje_71.vivienda_id || ' ' || v_mensaje_71.mensaje_respuesta_id);
+        dbms_output.put_line('Mensaje 72: ' || v_mensaje_72.mensaje_id || ' ' || v_mensaje_72.titulo || ' ' || v_mensaje_72.cuerpo || ' ' || v_mensaje_72.usuario_id || ' ' || v_mensaje_72.vivienda_id);
+    exception
+        when others then
+            if sqlcode = -20100 then
+                rollback;
+                dbms_output.put_line('Se está respondiendo a un mensaje que ya existe (ERROR)' || chr(10) || sqlerrm);
+            elsif sqlcode = -20200 then
+                rollback;
+                dbms_output.put_line('No existe el mensaje al que se está respondiendo (ERROR)' || chr(10) || sqlerrm);
+            elsif sqlcode = -20300 then
+                rollback;
+                dbms_output.put_line('Error al insertar el mensaje de respuesta (ERROR)' || chr(10) || sqlerrm);
+            else
+                rollback;
+                dbms_output.put_line('Error al insertar el mensaje de respuesta_1 (ERROR)' || chr(10) || sqlerrm);
+            end if;
+        rollback;
     end;
     rollback;
 end;
